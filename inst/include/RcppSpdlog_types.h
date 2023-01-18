5f2fe209a2e608d87ac4f5951e4bc6e846aec0fc
Dirk Eddelbuettel <edd@debian.org>
2023-01-17 18:46:51 -0600

    Stopwatch support without pulling fmt.h in

--- inst/include/RcppSpdlog_types.h
+++ inst/include/RcppSpdlog_types.h
@@ -2,4 +2,8 @@
 #pragma once
 
 #include <Rcpp/Lightest>
-#include <spdlog/stopwatch.h>   		// also support stopwatch feature
+#if defined(RCPPSPDLOG_STOPWATCH_NO_FMT)
+  #include <spdlog_stopwatch.h>	    	// spdlog::stopwatch without fmt header
+#else
+  #include <spdlog/stopwatch.h>   		// also support standard spdlog stopwatch feature
+#endif
