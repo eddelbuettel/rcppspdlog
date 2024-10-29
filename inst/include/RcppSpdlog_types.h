defcc524c2d9d69f25f97b4daf0452c9b5a77756
XanthosXanthopoulos <xanxanthopoulos@gmail.com>
2024-10-29 12:45:23 +0200

    Error out with approperiate message when using SPDLOG_USE_STD_FORMAT withput C++20 or greater

--- inst/include/RcppSpdlog_types.h
+++ inst/include/RcppSpdlog_types.h
@@ -1,6 +1,10 @@
 
 #pragma once
 
+#if defined(SPDLOG_USE_STD_FORMAT) && __cplusplus < 202002L
+#error SPDLOG_USE_STD_FORMAT should only be set when compiling with C++20 and greater
+#endif
+
 #include <Rcpp/Lightest>
 #if defined(RCPPSPDLOG_STOPWATCH_NO_FMT)
   #include <spdlog_stopwatch.h>	    	// spdlog::stopwatch without fmt header
